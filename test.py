f0da5125227bfffaa42d7210a079247b36d97e55
Johnathan Hewit <johnathan.hewit@live.com>
2019-04-29 14:53:06 -0700

    First commit - working algorithm on test cases.

---
+++ test.py
@@ -0,0 +1,50 @@
+from ete3 import Tree
+from ete3 import EvolTree
+
+#Map of Taxa to a 1 if anadromous and 0 if non-anadromous.
+anadromy = {'sea_lion': 0, 'seal': 0, 'monkey': 0, 'cat': 1, 'weasel': 1, 'dog': 1, 'raccoon': 0, 'bear': 1}
+
+tree = Tree("((raccoon:19.19959,bear:6.80041):0.84600,((sea_lion:11.99700, seal:12.00300):7.52973,((monkey:100.85930,cat:47.14069):20.59201, weasel:18.87953):2.09460):3.87382,dog:25.46154);")
+
+tree.resolve_polytomy()
+
+#Downpass for ASR
+for node in tree.traverse("postorder"):
+    if node.name in anadromy:
+        isAnadromous = set([anadromy[node.name]])
+        node.add_feature("anadromy", isAnadromous)
+
+        if node.up.name is "":
+            node.up.add_feature("anadromy", isAnadromous)
+            node.up.name = "visited"
+
+        elif anadromy[node.name] in node.up.anadromy:
+            node.up.add_feature("anadromy", node.anadromy.intersection(node.up.anadromy))
+
+        else:
+            node.up.add_feature("anadromy", node.up.anadromy.union(node.anadromy))
+
+    if node.name is "visited":
+        if not node.is_root():
+            if node.up.name is "":
+                node.up.add_feature("anadromy", node.anadromy)
+                node.up.name = "visited"
+
+            elif node.anadromy.issubset(node.up.anadromy) or node.anadromy.issuperset(node.up.anadromy):
+                node.up.add_feature("anadromy", node.up.anadromy.intersection(node.anadromy))
+
+            else:
+                node.up.add_feature("anadromy", node.up.anadromy.union(node.anadromy))
+
+#Up-pass for ASR
+for node in tree.traverse("preorder"):
+    if node.name is "visited":
+        if node.is_root():
+            node.anadromy #do something here to select node value
+        else:
+            if len(node.anadromy) > 1:
+                node.add_feature("anadromy", node.anadromy.intersection(node.up.anadromy))
+
+
+print(tree.get_ascii(attributes=["name", "anadromy"], show_internal=True)) #internal will show internal nodes, removing name will only show attributes
+tree.show()
