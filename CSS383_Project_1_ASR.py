a1099695893dd0780caeb82e24e6932839585b68
Johnathan Hewit <johnathan.hewit@live.com>
2019-05-02 23:14:54 -0700

    Added comments and UI

--- CSS383_Project_1_ASR.py
+++ CSS383_Project_1_ASR.py
@@ -6,7 +6,7 @@ class ASRTree:
     #Attributes
     __charStateChanges = 0
     __numOfTaxa = 0
-    __anadromyLookUp = dict()
+    __anadromyLookUp = dict() #Dictionary matching FASTA file names to taxa names and character states
     __tree = None
 
     #Constructor
@@ -14,7 +14,7 @@ class ASRTree:
         self.__tree = None
 
     #Public Methods
-    def buildTree(self, path):
+    def buildTree(self, path): #Builds newick tree from an aligned and filtered FASTA file
         raxFile = open(path, "r")
         if raxFile.mode == "r":
             contents = raxFile.read()
@@ -23,26 +23,26 @@ class ASRTree:
         else:
             print("\nRAxML tree failed to import successfully. Please check the file path and try again.")
 
-    def runMaxParsimony(self):
+    def runMaxParsimony(self): #Calls private functions for Fitch's algorithm of maximum parsimony
         if self.__tree is None:
             print("\n****************Error****************\nTree has not been imported. Please run buildTree method first.")
         else:
-            self.__tree.resolve_polytomy()
+            self.__tree.resolve_polytomy() #Transform tree to bifurcating
             self.__downPass()
             self.__upPass()
 
-    def getNumOfTaxa(self):
+    def getNumOfTaxa(self): #Returns the number of taxa
         return self.__numOfTaxa
 
-    def getCharStateChanges(self):
+    def getCharStateChanges(self): #Returns the number of character state changes
         return self.__charStateChanges
 
-    def importLookUp(self, path):
+    def importLookUp(self, path): #Imports the look-up file for assigning character state changes and taxa names
         importFile = xlrd.open_workbook(path)
         file = importFile.sheet_by_index(0)
-        values = list()
+        values = list() #Local list for holding cell row information
 
-        for row in range(1, file.nrows):
+        for row in range(1, file.nrows): #Nested loops to cover entire spreadsheet
             for col in range(file.ncols):
                 if col == 0:
                     fileName = file.cell_value(row, col)
@@ -63,10 +63,12 @@ class ASRTree:
 
     def showTree(self):
         print(self.__tree.get_ascii(attributes=["name", "anadromy"], show_internal=True))
+        self.__tree.show()
 
     #Private Methods
     def __downPass(self):
         for node in self.__tree.traverse("postorder"):
+            #Check for internal nodes that have been visted - marked as "Ancestor"
             if node.name is "Ancestor":
                 if not node.is_root():
                     if node.up.name is "":
@@ -95,15 +97,35 @@ class ASRTree:
                 node.name = self.__anadromyLookUp[node.name][1]
 
     def __upPass(self):
+        characterState = 0
         for node in self.__tree.traverse("preorder"):
             if node.name is "Ancestor":
                 if not node.is_root():
                     if len(node.anadromy) > 1:
                         node.add_feature("anadromy", node.anadromy.intersection(node.up.anadromy))
+                else:
+                    characterState = next(iter(node.anadromy))
 
+#Main - Driver for ASRTree
 newASR = ASRTree()
-path = ("C:/Users/johna/Google Drive/School/UW Bothell/CSS 383/Bioinformatics Team/Project 1/Python Resources/fish_anadromy.xlsx")
-newASR.importLookUp(path)
-newASR.buildTree("RAxML_bestTree.result")
-newASR.runMaxParsimony()
-newASR.showTree()
+userInput = 69
+print("\n\nWelcome to Anadromy Determinator 1000")
+input("\nPress any key to begin")
+while userInput != -1:
+    print("\n\n\tMain Menu")
+    userInput = int(input("\nChoose one of the following options:\n[1] Build Tree\n[2] Import Look-Up File\n[3] Run Maximum Parsimony\n[4] Display Tree\n[0] Exit Program\n\n"))
+    if userInput == 1:
+        newASR.buildTree("RAxML_bestTree.result")
+    elif userInput == 2:
+        path = ("C:/Users/johna/Google Drive/School/UW Bothell/CSS 383/Bioinformatics Team/Project 1/Python Resources/fish_anadromy.xlsx")
+        newASR.importLookUp(path)
+    elif userInput == 3:
+        newASR.runMaxParsimony()
+    elif userInput == 4:
+        newASR.showTree()
+    elif userInput == 0:
+        break
+    else:
+        print("\nInvalid Entry. Please try again.")
+
+print("\n\nThank you for using Anadromy Determinator 1000\n\n")
